f8b652dc2af83180b424d551da139eab0a419b4d
GUIHAL Maxime <sgp.jaconil@gmail.com>
2013-07-05 01:11:20 +0200

    #4 - Empêcher aux membres inscrits de voir les pages d'inscription, de login et de reset mot de passe

---
+++ src/Nantarena/UserBundle/Controller/AnonymousOnlyController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nantarena\UserBundle\Controller;
+
+interface AnonymousOnlyController
+{
+
+}
--- src/Nantarena/UserBundle/Controller/RegistrationController.php
+++ src/Nantarena/UserBundle/Controller/RegistrationController.php
@@ -2,37 +2,11 @@
 
 namespace Nantarena\UserBundle\Controller;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Nantarena\UserBundle\Controller\AnonymousOnlyController;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
 
 
-class RegistrationController extends BaseController
+class RegistrationController extends BaseController implements AnonymousOnlyController
 {
-    public function registerAction(Request $request)
-    {
-        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return new RedirectResponse($this->container->get('router')->generate('nantarena_site_home'));
-        }
-
-        return parent::registerAction($request);
-    }
-
-    public function checkEmailAction()
-    {
-        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return new RedirectResponse($this->container->get('router')->generate('nantarena_site_home'));
-        }
-
-        return parent::checkEmailAction();
-    }
-
-    public function confirmAction(Request $request, $token)
-    {
-        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return new RedirectResponse($this->container->get('router')->generate('nantarena_site_home'));
-        }
-
-        return parent::confirmAction($request, $token);
-    }
 }
