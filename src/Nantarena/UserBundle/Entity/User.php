955d0f9d700989706ec3cf0466afef3927e52ed0
GUIHAL Maxime <sgp.jaconil@gmail.com>
2013-07-11 00:08:41 +0200

    Ajout de la date d'inscription des utilisateurs

--- src/Nantarena/UserBundle/Entity/User.php
+++ src/Nantarena/UserBundle/Entity/User.php
@@ -2,8 +2,11 @@
 
 namespace Nantarena\UserBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\GroupInterface;
 use FOS\UserBundle\Model\User as BaseUser;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity
@@ -22,4 +25,87 @@ class User extends BaseUser
      * @ORM\ManyToMany(targetEntity="Nantarena\UserBundle\Entity\Group")
      */
     protected $groups;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime", name="registration_date")
+     * @Gedmo\Timestampable(on="create")
+     */
+    protected $registrationDate;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->groups = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set registrationDate
+     *
+     * @param \DateTime $registrationDate
+     * @return User
+     */
+    public function setRegistrationDate(\DateTime $registrationDate)
+    {
+        $this->registrationDate = $registrationDate;
+        return $this;
+    }
+
+    /**
+     * Get registrationDate
+     *
+     * @return \DateTime
+     */
+    public function getRegistrationDate()
+    {
+        return $this->registrationDate;
+    }
+
+    /**
+     * Add groups
+     *
+     * @param GroupInterface $groups
+     * @return User
+     */
+    public function addGroup(GroupInterface $groups)
+    {
+        $this->groups[] = $groups;
+
+        return $this;
+    }
+
+    /**
+     * Remove groups
+     *
+     * @param GroupInterface $groups
+     * @return User
+     */
+    public function removeGroup(GroupInterface $groups)
+    {
+        $this->groups->removeElement($groups);
+        return $this;
+    }
+
+    /**
+     * Get groups
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getGroups()
+    {
+        return $this->groups;
+    }
 }
