b4dc978052bbadd84ab15c87ecdf27a7053fce6a
KenTiN <quentin.lebourgeois@gmail.com>
2013-07-28 12:54:26 +0200

    #3 - Première itération optimisation requêtes

--- src/Nantarena/NewsBundle/Repository/NewsRepository.php
+++ src/Nantarena/NewsBundle/Repository/NewsRepository.php
@@ -3,6 +3,7 @@
 namespace Nantarena\NewsBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Nantarena\NewsBundle\Entity\Category;
 use Nantarena\NewsBundle\Entity\News;
 
@@ -13,25 +14,79 @@ use Nantarena\NewsBundle\Entity\News;
  */
 class NewsRepository extends EntityRepository
 {
+    public function getQueryBuilder()
+    {
+        return $this->createQueryBuilder('n');
+    }
+
+    public function addAuthor(QueryBuilder $qb)
+    {
+        $qb->join('n.author', 'a')->addSelect('a');
+
+        return $this;
+    }
+
+    public function addComments(QueryBuilder $qb)
+    {
+        $qb->join('n.comments', 'c')->addSelect('c');
+
+        return $this;
+    }
+
+    public function addCategory(QueryBuilder $qb)
+    {
+        $qb->join('n.category', 't')->addSelect('t');
+
+        return $this;
+    }
+
     public function findLatestPublished($limit = 5)
     {
-        return $this->findBy(array('state' => News::STATE_PUBLISHED), array(
-            'id' => 'desc',
-        ), $limit);
+        $qb = $this->getQueryBuilder();
+
+        $this->addAuthor($qb);
+        $this->addCategory($qb);
+        $this->addComments($qb);
+
+        $qb
+            ->addOrderBy('n.id', 'desc')
+            ->setMaxResults($limit);
+
+        return $qb->getQuery()->getResult();
     }
 
     public function findAllPublished($limit = null, $offset = null)
     {
-        return $this->findBy(array('state' => News::STATE_PUBLISHED), array(
-            'id' => 'desc',
-        ), $limit, $offset);
+        $qb = $this->getQueryBuilder();
+
+        $this->addAuthor($qb);
+        $this->addCategory($qb);
+        $this->addComments($qb);
+
+        $qb
+            ->addOrderBy('n.id', 'desc')
+            ->setMaxResults($limit)
+            ->setFirstResult($offset);
+
+        return $qb->getQuery()->getResult();
     }
 
     public function findAllPublishedByCategory(Category $category, $limit = null, $offset = null)
     {
-        return $this->findBy(array('category' => $category, 'state' => News::STATE_PUBLISHED), array(
-            'id' => 'desc',
-        ), $limit, $offset);
+        $qb = $this->getQueryBuilder();
+
+        $this->addAuthor($qb);
+        $this->addCategory($qb);
+        $this->addComments($qb);
+
+        $qb
+            ->andWhere('n.category = :category')
+            ->setParameter('category', $category)
+            ->addOrderBy('n.id', 'desc')
+            ->setMaxResults($limit)
+            ->setFirstResult($offset);
+
+        return $qb->getQuery()->getResult();
     }
 
     public function findAllOrderedByIdDesc()
@@ -39,14 +94,5 @@ class NewsRepository extends EntityRepository
         return $this->findBy(array(), array(
             'id' => 'desc',
         ));
-    }
-
-    public function countAllPublished()
-    {
-        return $this
-            ->createQueryBuilder('n')
-            ->select('count(n.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
     }
 }
