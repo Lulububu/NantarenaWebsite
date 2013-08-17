3ebee761b7e38709084d75f48aa774b7fea4fd73
GUIHAL Maxime <sgp.jaconil@gmail.com>
2013-08-17 18:17:15 +0200

    Filterable - Fix bug datepicker

--- src/Nantarena/AdminBundle/Resources/public/js/admin.js
+++ src/Nantarena/AdminBundle/Resources/public/js/admin.js
@@ -12,6 +12,7 @@ $(function() {
 
     $("table.filterable").bind('filterUpdate', function() {
         $('abbr, .ttip').tooltip();
+        datetime_init();
     });
 
     $("table.sortable").bind('sortEnd', function() {
